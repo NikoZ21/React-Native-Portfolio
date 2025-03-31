40e58d2d8f6b15b5274729145c7a8cdb153671e9
NikoZ21 <89398005+NikoZ21@users.noreply.github.com>
2025-03-31 23:18:57 +0400

    added stuff to Guessing game
    asdasda

--- GuessingGame/components/PrimaryButton.js
+++ GuessingGame/components/PrimaryButton.js
@@ -1,9 +1,43 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 
 export default function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log("Pressed");
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOutterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonOutterContainer: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    padding: 8,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- GuessingGame/components/StartGameScreen.js
+++ GuessingGame/components/StartGameScreen.js
@@ -4,20 +4,27 @@ import PrimaryButton from "./PrimaryButton";
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.input} maxLength={2} />
-      <PrimaryButton>Comfirm</PrimaryButton>
-      <PrimaryButton>Reset</PrimaryButton>
+      <TextInput style={styles.input} maxLength={2} keyboardType="number-pad" />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.button}>
+          <PrimaryButton>Comfirm</PrimaryButton>
+        </View>
+        <View style={styles.button}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
+    alignItems: "center",
     marginTop: 100,
-    marginHorizontal: 20,
+    marginHorizontal: 30,
     padding: 16,
     borderRadius: 6,
-    backgroundColor: "#72063c",
+    backgroundColor: "#4e0329",
     elevation: 8,
   },
 
@@ -29,5 +36,11 @@ const styles = StyleSheet.create({
     color: "#ddb52f",
     fontWeight: "bold",
     fontSize: 32,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  button: {
+    flex: 1,
   },
 });
